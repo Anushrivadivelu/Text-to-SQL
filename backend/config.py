567586838f0349cad725010b856dc572a1a95ce6
Sangamithradillibabu <sangamithra.dillibabu@gmail.com>
2025-12-29 14:23:10 +0530

    config.py

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / "database" / "text_to_sql.db"
 USER_DB_PATH = BASE_DIR / "database" / "user.db"
 
-OPENROUTER_API_KEY =" PUT API KEY HERE"
+OPENROUTER_API_KEY ="PUT API KEY HERE"
 OPENROUTER_MODEL = "gpt-4o-mini"
+
 
 
