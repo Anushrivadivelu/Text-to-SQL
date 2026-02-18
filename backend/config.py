24bdcf76c97c59f7fff95deae846f5ffcd3c49e9
Anushri Vadivelu <anushriv2007@gmail.com>
2026-02-18 11:07:43 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,8 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / "database" / "text_to_sql.db"
 USER_DB_PATH = BASE_DIR / "database" / "user.db"
 
-OPENROUTER_API_KEY ="PUT API KEY HERE"
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 OPENROUTER_MODEL = "gpt-4o-mini"
+
 
 
 
