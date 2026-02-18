c68b78810101553f7cb31082ce6bd08192fb8f43
Anushri Vadivelu <anushriv2007@gmail.com>
2026-02-18 11:09:18 +0530

    Update test.py

--- database/test.py
+++ database/test.py
@@ -1,9 +1,11 @@
-
 import sqlite3
+import os
 from pathlib import Path
 
-db = Path("database/user.db")
-conn = sqlite3.connect(db)
+BASE_DIR = Path(__file__).resolve().parent
+db_path = BASE_DIR / "database" / "user.db"
+
+conn = sqlite3.connect(str(db_path))
 cur = conn.cursor()
 
 cur.execute("SELECT * FROM users")
